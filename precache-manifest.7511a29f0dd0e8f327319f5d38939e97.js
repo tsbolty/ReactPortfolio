6489e9fe9189e3106e16896e3521b80e35e7e0ae
Tyler Bolty <tsbolty@gmail.com>
2020-05-26 09:47:13 -0600

    Updates

---
+++ precache-manifest.7511a29f0dd0e8f327319f5d38939e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61da35fa94e050bfa4e9fe88c6aba30f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bdad4029c6a08304b0a",
+    "url": "/static/css/main.afd588eb.chunk.css"
+  },
+  {
+    "revision": "d65ba624e4bf710956f6",
+    "url": "/static/js/2.9e2a4b98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e2a4b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bdad4029c6a08304b0a",
+    "url": "/static/js/main.7a997229.chunk.js"
+  },
+  {
+    "revision": "2490790c3d9e824a7faa",
+    "url": "/static/js/runtime-main.18bbefb9.js"
+  }
+]);
