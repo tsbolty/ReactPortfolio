8c897d466f15857c36e3672abf35f40d19d194ef
Tyler Bolty <tsbolty@gmail.com>
2020-06-02 09:12:47 -0600

    trying to fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Link } from "react"
 import Row from "./Row"
 
 function Navbar(){
@@ -8,19 +8,19 @@ function Navbar(){
             <div className="card-body card col-lg-2 offset-lg-1 col-md-2 offset-md-1 col-sm-2 offset-sm-4 col-xs-2 offset-xs-4">
                 <h1>Tyler Bolty</h1>
             </div>
-            <nav className="col-lg-5 offset-lg-4 col-md-5 offset-md-4 col-sm-12">
+          <nav className="col-lg-5 offset-lg-4 col-md-5 offset-md-4 col-sm-12">
             <ul className="nav justify-content-center">
                 <li className="nav-item">
-                  <a className="nav-link active" href="/">About</a>
+                  <Link to="/" className="nav-link active">About</Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/contact">Contact</a>
+                  <Link to="/contact" className="nav-link">Contact</Link>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/portfolio">Portfolio</a>
                 </li>
               </ul>
-        </nav>
+          </nav>
 
         </Row>
 
